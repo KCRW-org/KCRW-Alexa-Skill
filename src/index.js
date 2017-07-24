5d6a066e2931e8c97a035216be406a329c6238d2
Alec Mitchell <alecpm@gmail.com>
2017-07-23 22:19:23 -0700

    Oops.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 'use strict';
 
 var Alexa = require('alexa-sdk');
-var APP_ID = "amzn1.ask.skill.20fd9b6b-2754-40e7-a485-";
+var APP_ID = "amzn1.ask.skill.20fd9b6b-2754-40e7-a485-95152e19a42b";
 var request = require('request');
 
 
