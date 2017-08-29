b3ec8fc50a79830008620b7cc620dc4bd422b958
Alec Mitchell <alecpm@gmail.com>
2017-08-28 20:20:34 -0700

    Add title card and show announcement on initial play.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
 exports.APP_ID = "amzn1.ask.skill.aba132da-0ac4-4dc1-9e49-a8a1d7af7cc8";
+exports.CHANNEL_TEXT = {
+    "live": "k.c.r.w.",
+    "music": "Eclectic Twenty Four",
+    "news": "News Twenty Four"
+}
